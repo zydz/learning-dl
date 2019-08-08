b5316f68ad04d578b526eb6dce99a6f332c122a2
yuding zhou <zzyd@msn.com>
2019-08-08 16:55:35 +0800

    add handout example

---
+++ misc/my-handout-example.py
@@ -0,0 +1,66 @@
+import handout
+doc = handout.Handout('output2')
+doc.show()
+
+
+"""
+# Test Handout
+---
+this is my first test of handout
+---
+
+"""
+
+"""
+## List
+- one
+- two
+- __emphersize__
+- ~~delete~~
+"""
+
+"""
+## Link
+- Show a image here ![image]()
+- Show a link [Link](www.baidu.com)
+"""
+
+"""
+## Task lists
+- [x] this is a completed item
+- [ ] this is a incompleted item
+- [x] @mention
+- #refer
+- [link]()
+- **formatting**
+- <del>delete</del>
+
+"""
+
+"""
+## Table
+First Header | Second Header
+--- | ---
+Cell11 | Cell 12
+Cell21 | Cell 22
+"""
+
+"""
+## JavaScript Style Table
+<table style="width:50%">
+  <tr>
+    <td>a11 </td>
+    <td>a12 </td>
+  </tr>
+  <tr>
+    <td>a21 </td>
+    <td>a22 </td>
+  </tr>
+</table>
+"""
+
+
+"""
+## LaTex (Unsupport)
+$$x^n+y^n=z^n \eqno{(1)}$$
+"""
